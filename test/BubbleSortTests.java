ae52a0c7042dfec0ae78eb66a9071e3ebb8fbd50
jasongorman <jason.gorman@codemanship.com>
2016-03-28 10:38:45 +0100

    Update BubbleSortTests.java

--- test/BubbleSortTests.java
+++ test/BubbleSortTests.java
@@ -2,13 +2,6 @@
 
 public class BubbleSortTests extends SortTests {
 
-<<<<<<< HEAD
-=======
-	public BubbleSortTests(int[] input, int[] expectedOutput) {
-		super(input, expectedOutput);
-	}
-
->>>>>>> origin/master
 	@Override
 	protected Sort createSort() {
 		return new BubbleSort();
